2eaf36c9ef9cb3185c6486369baaa42d6d61d846
Minh Phan <minh.phan@qwconsult.com>
2017-06-29 15:34:02 -0700

    Components

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'; //core functionalities of Angular
+
+@Component({
+ selector: 'app-server', 
+ templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
