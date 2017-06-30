e42bf9d3792c2100a030adef2be0b7d2c2d99b16
Minh Phan <minh.phan@qwconsult.com>
2017-06-30 16:13:05 -0700

    learn everything before creating basic project

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,14 +2,27 @@ import { Component } from '@angular/core'; //core functionalities of Angular
 
 @Component({
  selector: 'app-server', 
- templateUrl: './server.component.html'
+ templateUrl: './server.component.html',
+ styles: [`
+    .online {
+        color: white;
+    }
+ `]
 })
 
 export class ServerComponent {
     severId: number = 10; 
     serverStatus: string = 'offline'
 
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
     getServerStatus () {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red'; 
     }
 }
